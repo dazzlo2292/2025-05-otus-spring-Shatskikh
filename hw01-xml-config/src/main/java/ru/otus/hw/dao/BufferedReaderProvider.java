8c217660913e34e51451767b9b843f6d2ad73ac5
Nikolay Shatskikh <nikolay.shatskikh@edna.ru>
2025-06-13 12:50:26 +0300

    fix checkstyle

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/BufferedReaderProvider.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/BufferedReaderProvider.java
@@ -2,7 +2,11 @@ package ru.otus.hw.dao;
 
 import lombok.RequiredArgsConstructor;
 
-import java.io.*;
+import java.io.Reader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
 
 @RequiredArgsConstructor
