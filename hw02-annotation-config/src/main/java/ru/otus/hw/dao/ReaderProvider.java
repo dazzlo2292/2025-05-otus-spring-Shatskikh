086ed3c35977940a5582890243d80b131f41f0db
Nikolay Shatskikh <nikolay.shatskikh@edna.ru>
2025-06-25 17:18:23 +0300

    fix daoTest and exceptions

--- hw02-annotation-config/src/main/java/ru/otus/hw/dao/ReaderProvider.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.hw.dao;
-
-import java.io.FileNotFoundException;
-import java.io.Reader;
-
-public interface ReaderProvider {
-    Reader getReader(String fileName) throws FileNotFoundException;
-}
