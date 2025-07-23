a5ce1c811125e650d84ac68cd7da2201ccf3192e
Nikolay Shatskikh <nikolay.shatskikh@edna.ru>
2025-07-23 12:10:20 +0300

    homework-7 completed

---
+++ hw07-spring-data/src/main/java/ru/otus/hw/models/Book.java
@@ -0,0 +1,44 @@
+package ru.otus.hw.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.NamedEntityGraph;
+import jakarta.persistence.NamedAttributeNode;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.FetchType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "books")
+@NamedEntityGraph(name = "book-author-genre-entity-graph",
+        attributeNodes = {@NamedAttributeNode("author"),
+                          @NamedAttributeNode("genre")})
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title", nullable = false, unique = true)
+    private String title;
+
+    @ManyToOne(targetEntity = Author.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    @ManyToOne(targetEntity = Genre.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+}
---
+++ hw07-spring-data/src/test/java/ru/otus/hw/services/BookServiceTest.java
@@ -0,0 +1,55 @@
+package ru.otus.hw.services;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+
+@DisplayName("Сервис для работы с книгами ")
+@DataJpaTest
+@Import({BookServiceImpl.class})
+public class BookServiceTest {
+
+    @Autowired
+    private BookService bookService;
+
+    private static final long BOOK_ID = 1L;
+
+    @DisplayName("должен загружать все связи книги при запросе по id")
+    @Test
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    void shouldNotThrowLazyExceptionForFindBookById() {
+        assertDoesNotThrow(() -> bookService
+                .findById(BOOK_ID)
+                .get()
+                .getAuthor().getFullName());
+
+        assertDoesNotThrow(() -> bookService
+                .findById(BOOK_ID)
+                .get()
+                .getGenre().getName());
+    }
+
+    @DisplayName("должен загружать все связи книги при запросе всех книг")
+    @Test
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    void shouldNotThrowLazyExceptionForFindAllBooks() {
+        assertDoesNotThrow(() -> bookService
+                .findAll()
+                .get(0)
+                .getAuthor()
+                .getFullName());
+
+        assertDoesNotThrow(() -> bookService
+                .findAll()
+                .get(0)
+                .getGenre()
+                .getName());
+    }
+}
