265a3fe1531dde122f342762ade289407b2b9e81
Nikolay Shatskikh <dazzlo2292@mail.ru>
2025-08-10 11:20:44 +0300

    fixes after review

--- hw10-ajax/src/main/java/ru/otus/hw/page/BookPagesController.java
+++ hw10-ajax/src/main/java/ru/otus/hw/page/BookPagesController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import ru.otus.hw.rest.dto.AuthorDto;
 import ru.otus.hw.rest.dto.BookDto;
 import ru.otus.hw.rest.dto.GenreDto;
-import ru.otus.hw.rest.exceptions.EntityNotFoundException;
 import ru.otus.hw.services.AuthorService;
-import ru.otus.hw.services.BookService;
 import ru.otus.hw.services.GenreService;
 
 import java.util.List;
@@ -18,8 +16,6 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 public class BookPagesController {
-
-    private final BookService bookService;
 
     private final AuthorService authorService;
 
@@ -32,10 +28,6 @@ public class BookPagesController {
 
     @GetMapping("/edit/book")
     public String editBookPage(@RequestParam("id") long id, Model model) {
-        BookDto book = bookService.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Book with id %d not found".formatted(id)));
-
-        model.addAttribute("book", book);
         model.addAttribute("bookId", id);
 
         List<AuthorDto> authors = authorService.findAll();
